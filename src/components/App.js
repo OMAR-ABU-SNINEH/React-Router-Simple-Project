de006c2211bb21b77419612282ee205c6a01eb4a
OMAR-ABU-SNINEH <ooommmaaarrr548@gmail.com>
2023-08-13 16:03:49 +0300

    with app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Main from "./Main";
 import Home from "../pages/Home";
+import { Routes, Route } from "../../node_modules/react-router-dom/dist/index";
+import Css from "../pages/Css";
+import Html from "../pages/Html";
+import JavaScript from "../pages/JavaScript";
 
 class App extends React.Component {
   render() {
-    return <></>;
+    return (
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/html" element={<Html />} />
+          <Route path="/css" element={<Css />} />
+          <Route path="/javascript" element={<JavaScript />} />
+        </Routes>
+      </>
+    );
   }
 }
 export default App;
